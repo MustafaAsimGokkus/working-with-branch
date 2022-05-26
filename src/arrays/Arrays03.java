8c843c85492b4978baface4e908e4f7d82af8e92
Asim Gokkus <asimgokkus@gmail.com>
2022-05-26 18:06:11 +0200

    zeros of the array sent to the end of the array

--- src/arrays/Arrays03.java
+++ src/arrays/Arrays03.java
@@ -7,9 +7,19 @@ public class Arrays03 {
     public static void main(String[] args) {
 
      int []arr = new int [6];
-        //put zeros to the end
+     //put zeros to the end
      arr[0]=0; arr[1]=2; arr[2]=3; arr[3]=0; arr[4]=12; arr[5]=0;
+     int idx = 0;
 
-     int []sorted = Arrays.stream(arr).sorted();
+
+     int [] brr = new int[arr.length];
+     for(int i=0; i<arr.length; i++){
+        if(arr[i] != 0){
+          brr[idx]=arr[i];
+          idx++;
+        }
+     }
+
+        System.out.println(Arrays.toString(brr));
     }
 }
